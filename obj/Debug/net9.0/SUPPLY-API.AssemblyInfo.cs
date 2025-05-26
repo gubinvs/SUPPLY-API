e8fc7e62e6b4c1f9bfc7de7a606090f1ead9cf8c
gubinvs@gmail.com <gubinvs@gmail.com>
2025-05-26 11:14:10 +0300

    delete class

--- obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab761d6cbb74e9273386f405f16c56a23a43376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eceb86417f653fa2d1aeaecf93d4daa940654577")]
 [assembly: System.Reflection.AssemblyProductAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

11c83b103a005fd07cc1a69e1476f8541351290b
master <gubinvs@gmail.com>
2025-06-04 18:25:30 +0300

    d

--- Application/EmailCleanupHostedService.cs
+++ Application/EmailCleanupHostedService.cs
@@ -1,64 +1,64 @@
-// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Hosting;
-// using Microsoft.Extensions.Logging;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
-// /// Класс с фоновой задачей по запуску удаления не подтвержденных email
+/// Класс с фоновой задачей по запуску удаления не подтвержденных email
 
-// namespace encomponent.api.Services
-// {
-//     public class EmailCleanupHostedService : BackgroundService
-//     {
-//         private readonly IServiceProvider _serviceProvider;
-//         private readonly ILogger<EmailCleanupHostedService> _logger;
+namespace SUPPLY_API 
+{
+    public class EmailCleanupHostedService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<EmailCleanupHostedService> _logger;
 
-//         public EmailCleanupHostedService(IServiceProvider serviceProvider, ILogger<EmailCleanupHostedService> logger)
-//         {
-//             _serviceProvider = serviceProvider;
-//             _logger = logger;
-//         }
+        public EmailCleanupHostedService(IServiceProvider serviceProvider, ILogger<EmailCleanupHostedService> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
 
-//         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-//         {
-//             _logger.LogInformation("Email cleanup hosted service started.");
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Email cleanup hosted service started.");
 
-//             while (!stoppingToken.IsCancellationRequested)
-//             {
-//                 try
-//                 {
-//                     using (var scope = _serviceProvider.CreateScope())
-//                     {
-//                         var dbContext = scope.ServiceProvider.GetRequiredService<UserSystemContext>();
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var dbContext = scope.ServiceProvider.GetRequiredService<CollaboratorSystemContext>();
 
-//                         var thresholdTime = DateTime.UtcNow.AddHours(-48);
-//                         var usersToRemove = await dbContext.UserSystem
-//                             .Where(u => !u.IsConfirmed && u.DataReg < thresholdTime)
-//                             .ToListAsync(stoppingToken);
+                        var thresholdTime = DateTime.UtcNow.AddHours(-48);
+                        var usersToRemove = await dbContext.CollaboratorSystem
+                            .Where(u => !u.ActivationEmailCollaborator && u.DataRegistrationCollaborator < thresholdTime)
+                            .ToListAsync(stoppingToken);
 
-//                         if (usersToRemove.Any())
-//                         {
-//                             dbContext.UserSystem.RemoveRange(usersToRemove);
-//                             await dbContext.SaveChangesAsync(stoppingToken);
-//                             _logger.LogInformation($"Removed {usersToRemove.Count} unconfirmed users.");
-//                         }
-//                         else
-//                         {
-//                             _logger.LogInformation("No unconfirmed users to remove.");
-//                         }
-//                     }
+                        if (usersToRemove.Any())
+                        {
+                            dbContext.CollaboratorSystem.RemoveRange(usersToRemove);
+                            await dbContext.SaveChangesAsync(stoppingToken);
+                            _logger.LogInformation($"Removed {usersToRemove.Count} unconfirmed users.");
+                        }
+                        else
+                        {
+                            _logger.LogInformation("No unconfirmed users to remove.");
+                        }
+                    }
 
-//                     // Ожидание до следующей полуночи
-//                     var currentTime = DateTime.UtcNow;
-//                     var nextRunTime = currentTime.Date.AddDays(1);
-//                     var delay = nextRunTime - currentTime;
-//                     await Task.Delay(delay, stoppingToken);
-//                 }
-//                 catch (Exception ex)
-//                 {
-//                     _logger.LogError(ex, "Error occurred during email cleanup.");
-//                     await Task.Delay(TimeSpan.FromHours(1), stoppingToken); // Перезапуск через час в случае ошибки
-//                 }
-//             }
-//         }
-//     }
-// }
+                    // Ожидание до следующей полуночи
+                    var currentTime = DateTime.UtcNow;
+                    var nextRunTime = currentTime.Date.AddDays(1);
+                    var delay = nextRunTime - currentTime;
+                    await Task.Delay(delay, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error occurred during email cleanup.");
+                    await Task.Delay(TimeSpan.FromHours(1), stoppingToken); // Перезапуск через час в случае ошибки
+                }
+            }
+        }
+    }
+}
--- Application/PasswordGenerator.cs
+++ Application/PasswordGenerator.cs
@@ -1,29 +1,29 @@
-// using System;
-// using System.Text;
+using System;
+using System.Text;
 
-// public class PasswordGenerator
-// {
-//     private static readonly string ValidChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;:,.<>?";
+public class PasswordGenerator
+{
+    private static readonly string ValidChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-//     public static string GeneratePassword(int length)
-//     {
-//         Random rand = new Random();
-//         StringBuilder password = new StringBuilder(length);
+    public static string GeneratePassword(int length)
+    {
+        Random rand = new Random();
+        StringBuilder password = new StringBuilder(length);
 
-//         for (int i = 0; i < length; i++)
-//         {
-//             // Выбираем случайный символ из допустимых символов
-//             password.Append(ValidChars[rand.Next(ValidChars.Length)]);
-//         }
+        for (int i = 0; i < length; i++)
+        {
+            // Выбираем случайный символ из допустимых символов
+            password.Append(ValidChars[rand.Next(ValidChars.Length)]);
+        }
 
-//         return password.ToString();
-//     }
+        return password.ToString();
+    }
 
-//     // Реализация в контроллере
-//     // public static void Main()
-//     // {
-//     //     int passwordLength = 12; // Указываем длину пароля
-//     //     string password = GeneratePassword(passwordLength);
-//     //     //Console.WriteLine("Generated Password: " + password);
-//     // }
-// }
+    // Реализация в контроллере
+    // public static void Main()
+    // {
+    //     int passwordLength = 12; // Указываем длину пароля
+    //     string password = GeneratePassword(passwordLength);
+    //     //Console.WriteLine("Generated Password: " + password);
+    // }
+}

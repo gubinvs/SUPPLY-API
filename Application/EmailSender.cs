f2c4bb0d80ea13983282dfa8b4072447f79728e7
gubinvs@gmail.com <gubinvs@gmail.com>
2025-06-05 10:20:54 +0300

    e

--- Application/EmailSender.cs
+++ Application/EmailSender.cs
@@ -1,55 +1,57 @@
 using System;
 using System.Net;
 using System.Net.Mail;
+namespace SUPPLY_API {
 
-public class EmailSender
-{
-    private string smtpServer; // Например, для Gmail это "smtp.gmail.com"
-    private int port; // Порт(для Gmail 587)
-    private string fromEmail; // Ваш email
-    private string password; // Ваш пароль (не храните пароль в коде!)
-
-    // Конструктор для инициализации данных
-    //public EmailSender(string smtpServer, int port, string fromEmail, string password)
-    public EmailSender()
+    public class EmailSender
     {
-        this.smtpServer = "smtp.beget.com";
-        this.port = 2525;
-        this.fromEmail = "support@encomponent.ru";
-        this.password = "JRk*upFTqk5b";
-    }
+        private string smtpServer; // Например, для Gmail это "smtp.gmail.com"
+        private int port; // Порт(для Gmail 587)
+        private string fromEmail; // Ваш email
+        private string password; // Ваш пароль (не храните пароль в коде!)
 
-    /// <summary>
-    /// Метод для отправки сообщения
-    /// </summary>
-    /// <param name="toEmail">Кому отправляем</param>
-    /// <param name="subject">Заголовок письма</param>
-    /// <param name="body">Сообщение</param>
-    /// <returns></returns>
-    public bool SendEmail(string toEmail, string subject, string body)
-    {
-        try
+        // Конструктор для инициализации данных
+        //public EmailSender(string smtpServer, int port, string fromEmail, string password)
+        public EmailSender()
         {
-            // Создание клиента SMTP
-            SmtpClient smtpClient = new SmtpClient(smtpServer)
-            {
-                Port = port,
-                Credentials = new NetworkCredential(fromEmail, password),
-                EnableSsl = true
-            };
-
-            // Создание сообщения
-            MailMessage mailMessage = new MailMessage(fromEmail, toEmail, subject, body);
-
-            // Отправка сообщения
-            smtpClient.Send(mailMessage);
-
-            return true; // Если отправлено успешно
+            this.smtpServer = EmailSenderEncomponentConnect.smtpServer;
+            this.port = EmailSenderEncomponentConnect.port;
+            this.fromEmail = EmailSenderEncomponentConnect.fromEmail;
+            this.password = EmailSenderEncomponentConnect.password;
         }
-        catch (Exception ex)
+
+        /// <summary>
+        /// Метод для отправки сообщения
+        /// </summary>
+        /// <param name="toEmail">Кому отправляем</param>
+        /// <param name="subject">Заголовок письма</param>
+        /// <param name="body">Сообщение</param>
+        /// <returns></returns>
+        public bool SendEmail(string toEmail, string subject, string body)
         {
-            Console.WriteLine("Ошибка при отправке письма: " + ex.Message);
-            return false; // Если произошла ошибка
+            try
+            {
+                // Создание клиента SMTP
+                SmtpClient smtpClient = new SmtpClient(smtpServer)
+                {
+                    Port = port,
+                    Credentials = new NetworkCredential(fromEmail, password),
+                    EnableSsl = true
+                };
+
+                // Создание сообщения
+                MailMessage mailMessage = new MailMessage(fromEmail, toEmail, subject, body);
+
+                // Отправка сообщения
+                smtpClient.Send(mailMessage);
+
+                return true; // Если отправлено успешно
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при отправке письма: " + ex.Message);
+                return false; // Если произошла ошибка
+            }
         }
     }
 }

a1aaf174635625aaf72b7adc8df9e4c7850cbbf1
gubinvs@gmail.com <gubinvs@gmail.com>
2025-06-10 12:44:48 +0300

    sd

--- Controllers/AddInformationCompany.cs
+++ Controllers/AddInformationCompany.cs
@@ -1,9 +1,10 @@
+using System.Data.Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SUPPLY_API
 {
     /// <summary>
-    /// Контроллер для добавления информации о новой компании в базу данных
+    /// Контроллер для добавления в базу данных или изменения информации о новой компании
     /// </summary>
     /// 
 
@@ -11,13 +12,21 @@ namespace SUPPLY_API
     [Route("api/[controller]")]
     public class AddInformationCompanyController : ControllerBase
     {
-        public IActionResult AddInformationCompany([FromBody] AddInformationCompanyModel model)
+        public IActionResult AddInformationCompany([FromBody] InformationCompanyModel model)
         {
-            return Ok();
+            //Подключаемся к базе данных
+            using (var _db = new SupplyCompanyContext())
+            {
+                // Проверяем информацию в базе данных о наличии записи с GuidIdCompany
+                var company = _db.SupplyCompany
+                        .Where(p => p.GuidIdCompany == model.GuidIdCompany);
 
+                return Ok(company);
+            }
+
+            
+            
         }
 
     }
-    
-    public record AddInformationCompanyModel(string Email, string Password, string GuidIdRoleSystem);
 }
---
+++ Models/InformationCompanyModel.cs
@@ -0,0 +1,20 @@
+
+namespace SUPPLY_API
+{
+    /// <summary>
+    /// Модель передачи в контроллер новых или измененых данных о компании
+    /// </summary>
+    /// <param name="GuidIdCompany">Идентификатор компании</param>
+    /// <param name="FullNameCompany">Полное наименование компании</param>
+    /// <param name="AbbreviatedNameCompany">Сокращенное наименование компании</param>
+    /// <param name="InnCompany">ИНН компании</param>
+    /// <param name="AddressCompany">Юридический адрес компании</param>
+    public record InformationCompanyModel
+    (
+        string GuidIdCompany,
+        string FullNameCompany,
+        string AbbreviatedNameCompany,
+        long InnCompany,
+        string AddressCompany
+    );
+};

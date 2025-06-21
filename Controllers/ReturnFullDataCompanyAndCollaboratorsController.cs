6b34ebfd83533664bb1da33a7998c1beb88fabb2
master <gubinvs@gmail.com>
2025-06-21 23:14:21 +0300

    sd

---
+++ Controllers/ReturnFullDataCompanyAndCollaboratorsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SUPPLY_API
+{
+    /// <summary>
+    /// Собирает и возвращает полную информацию о компании на основании ИНН 
+    /// и информацию о менеджерах которые приклеплены к этой компании
+    /// </summary>
+    public class ReturnFullDataCompanyAndCollaboratorsController : ControllerBase
+    {
+
+    }
+}
--- obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b11428d11171f80b879b01f173314f53f23beb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47228f84aa8842dfea4efaa3786780307bda3e7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

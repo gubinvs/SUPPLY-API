331831441edb0ba94283707fa32cebe7312ab43a
master <gubinvs@gmail.com>
2025-06-21 19:23:30 +0300

    as

--- Core/CurrentServer.cs
+++ Core/CurrentServer.cs
@@ -5,16 +5,16 @@ namespace SUPPLY_API
     public static class CurrentServer
     {
 
-        //public static string ServerAddress = "http://localhost:8080"; // Локальный сервер для тренировок
+        public static string ServerAddress = "http://localhost:8080"; // Локальный сервер API
 
         // Сервер на котором крутиться frontend
-        //public static string ServerAddressFrontend = "http://localhost:3000";  // Локальный сервер для тренировок
+        public static string ServerAddressFrontend = "http://localhost:3000";  // Локальный сервер для тренировок
 
 
         // Адрес на котором лежит этот API сервер
-        public static string ServerAddress = "http://31.129.97.48:1030"; // Боевой сервер api
+        //public static string ServerAddress = "http://31.129.97.48:1030"; // Боевой сервер API
 
         // Сервер на котором крутиться frontend
-        public static string ServerAddressFrontend = "http://31.129.97.48:1040";  // Боевой сервер frond
+        //public static string ServerAddressFrontend = "http://31.129.97.48:1040";  // Боевой сервер frond
     }
 }

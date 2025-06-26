78adf83c05041c4bad8df335dcbd082fee0ae06c
master <gubinvs@gmail.com>
2025-06-26 22:34:53 +0300

    ы

--- Application/EmailSender.cs
+++ Application/EmailSender.cs
@@ -1,56 +1,38 @@
 using System;
 using System.Net;
 using System.Net.Mail;
-namespace SUPPLY_API {
 
+namespace SUPPLY_API
+{
     public class EmailSender
     {
-        private string smtpServer; // Например, для Gmail это "smtp.gmail.com"
-        private int port; // Порт(для Gmail 587)
-        private string fromEmail; // Ваш email
-        private string password; // Ваш пароль (не храните пароль в коде!)
+        private readonly EmailSettings _settings;
 
-        // Конструктор для инициализации данных
-        //public EmailSender(string smtpServer, int port, string fromEmail, string password)
-        public EmailSender()
+        public EmailSender(EmailSettings settings)
         {
-            this.smtpServer = EmailSenderSistemautorConnect.smtpServer;
-            this.port = EmailSenderSistemautorConnect.port;
-            this.fromEmail = EmailSenderSistemautorConnect.fromEmail;
-            this.password = EmailSenderSistemautorConnect.password;
+            _settings = settings;
         }
 
-        /// <summary>
-        /// Метод для отправки сообщения
-        /// </summary>
-        /// <param name="toEmail">Кому отправляем</param>
-        /// <param name="subject">Заголовок письма</param>
-        /// <param name="body">Сообщение</param>
-        /// <returns></returns>
         public bool SendEmail(string toEmail, string subject, string body)
         {
             try
             {
-                // Создание клиента SMTP
-                SmtpClient smtpClient = new SmtpClient(smtpServer)
+                var smtpClient = new SmtpClient(_settings.SmtpServer)
                 {
-                    Port = port,
-                    Credentials = new NetworkCredential(fromEmail, password),
+                    Port = _settings.Port,
+                    Credentials = new NetworkCredential(_settings.FromEmail, _settings.Password),
                     EnableSsl = true
                 };
 
-                // Создание сообщения
-                MailMessage mailMessage = new MailMessage(fromEmail, toEmail, subject, body);
-
-                // Отправка сообщения
+                var mailMessage = new MailMessage(_settings.FromEmail, toEmail, subject, body);
                 smtpClient.Send(mailMessage);
 
-                return true; // Если отправлено успешно
+                return true;
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Ошибка при отправке письма: " + ex.Message);
-                return false; // Если произошла ошибка
+                return false;
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,106 +1,59 @@
 using Microsoft.EntityFrameworkCore;
-using SUPPLY_API; // если SupplyComponentContext и модель в этом пространстве имён
-using SUPPLY_API.Models;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.IO;
-using System.Threading.Tasks;
+using System.Text;
+using SUPPLY_API;
+using SUPPLY_API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Настройка защиты данных
+// === Настройка защиты данных ===
 var keysDirectory = new DirectoryInfo("/app/keys");
 var dataProtectionBuilder = builder.Services.AddDataProtection().PersistKeysToFileSystem(keysDirectory);
 
-// Используем шифрование, если работаем в Windows
 if (OperatingSystem.IsWindows())
 {
     dataProtectionBuilder.ProtectKeysWithDpapiNG();
 }
 
-// Установка порта из переменных окружения или по умолчанию
+// === Установка порта ===
 var port = Environment.GetEnvironmentVariable("HTTP_PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://+:{port}");
 
-// Добавление сервисов
+// === Сервисы ===
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// === Фоновые службы ===
 builder.Services.AddHostedService<EmailCleanupHostedService>();
+builder.Services.AddHostedService<DuplicateCleanupService>();
+
+// === Email настройки ===
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
-builder.Services.AddHostedService<DuplicateCleanupService>(); // Удаляет раз в сутки дубли номенклатуры
+builder.Services.AddScoped<EmailSender>();
 
+// === Строка подключения (одна на все контексты) ===
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
 
-builder.Services.AddDbContext<UnitMeasurementComponentContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
+// === DbContext-ы ===
+builder.Services.AddDbContext<UnitMeasurementComponentContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyUnitMeasurementContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyProviderContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyPriceComponentContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyManufacturerContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyComponentContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<SupplyCompanyContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<CollaboratorSystemContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<CompanyCollaboratorContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<DeliveryAddressContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.AddDbContext<ManufacturerComponentContext>(options => options.UseMySql(connectionString, serverVersion));
 
-builder.Services.AddDbContext<SupplyUnitMeasurementContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<SupplyProviderContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<SupplyPriceComponentContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<SupplyManufacturerContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<SupplyComponentContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<SupplyCompanyContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-builder.Services.AddDbContext<CollaboratorSystemContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-builder.Services.AddDbContext<CompanyCollaboratorContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-builder.Services.AddDbContext<DeliveryAddressContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-builder.Services.AddDbContext<ManufacturerComponentContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 25)));
-});
-
-
-
-// Настройка JWT
-var secretKey = "YourSecureKeyHereMustBeLongEnough";
+// === JWT-аутентификация ===
+var secretKey = "YourSecureKeyHereMustBeLongEnough"; // Вынести в конфигурацию!
 var key = Encoding.UTF8.GetBytes(secretKey);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -116,21 +69,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-// Добавление CORS
+// === CORS ===
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .SetIsOriginAllowed(_ => true);
-        });
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed(_ => true);
+    });
 });
 
 var app = builder.Build();
 
-// Настройка middleware
+// === Middleware ===
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -138,8 +90,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.UseAuthentication(); // <-- Обязательно до Authorization
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -147,9 +105,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-app.UseCors("AllowAll");
 app.Run();
+

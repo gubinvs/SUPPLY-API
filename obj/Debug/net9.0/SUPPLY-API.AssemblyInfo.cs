76f6f7d7af2b107bd4740fb4417cfb728a864583
master <gubinvs@gmail.com>
2025-08-25 21:57:25 +0300

    c

--- obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/SUPPLY-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a2ab80535f6e1e9c80af94f4275b9d2a7735aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d44c303b1851c6a1479f1d7d61cd475b0cd6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SUPPLY-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

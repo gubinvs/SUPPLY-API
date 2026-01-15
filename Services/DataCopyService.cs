18f9e3841ede7773c6d7629df69ad5dfa95ea02d
master <gubinvs@gmail.com>
2026-01-15 17:59:20 +0300

    р

--- Services/DataCopyService.cs
+++ Services/DataCopyService.cs
@@ -61,7 +61,7 @@ using Microsoft.Extensions.Hosting;
             public Task StartAsync(CancellationToken cancellationToken)
             {
                 var now = DateTime.Now;
-                var nextRun = new DateTime(now.Year, now.Month, now.Day, 13, 01, 0); // каждый день в 7:00
+                var nextRun = new DateTime(now.Year, now.Month, now.Day, 17, 51, 0); // каждый день в 7:00
                 var delay = nextRun - now;
                 if (delay.TotalMilliseconds < 0)
                     delay = delay.Add(TimeSpan.FromDays(1));
